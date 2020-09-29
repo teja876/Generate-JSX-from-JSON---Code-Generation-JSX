97a28aab89687642b3118c2d30a482a8d93cec8f
HSM256 <harsha.katakam123@gmail.com>
2020-09-29 23:26:49 +0530

    updating the code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+
+let answer = "";
+
+
+function camelize(str) {
+    return str
+      .replace(/\w+/g, function (word, index) {
+        return index === 0
+          ? word.toLowerCase()
+          : word[0].toUpperCase() + word.slice(1);
+      })
+      .replace(/[-]/g, "");
+  }
+
+function addStyle(obj){
+    if(obj === undefined || Object.keys(obj).length() === 0) return;
+    answer += " style={{";
+    for(const [key, value] of Object.entries(obj)){
+        answer += key + ":";
+        answer += `"${camelize(value)}",`;
+    }
+    answer.slice(0, -1);
+    answer += "}}";
+}
+
+function formString(obj){
+    answer += `<${obj.name}`;
+    addStyle(obj.style);
+    let children = obj.children;
+    if(children.length() === 0) {
+        answer += "/>";
+        return;
+    }
+    else{
+        answer += ">\n";
+        for(let i = 0; i < children.length(); i++){
+            formString(children[i]);
+        }
+    }
+    answer += "</" + obj.name + ">";
+}
+
 function generateCodeFromObject(obj){
+    formString(obj);
+    return answer;
     //return a code generated string
    }
    
